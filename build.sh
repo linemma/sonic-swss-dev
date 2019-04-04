cc7f20db558c653e1a37d5ad220dda301853fc39
JunYing Yeh <junying_yeh@edge-core.com>
2019-04-05 07:15:26 +0800

    using sed instead 'git patch' that always fail if someone make a new commit

--- build.sh
+++ build.sh
@@ -7,14 +7,16 @@ SRC_PATH=$(realpath $(dirname "$0"))
 echo "SRC path  ="$SRC_PATH
 echo "Build path="${BUILD_PATH}
 
-$DIR/install-pkg.sh && . $BUILD_PATH/packages/.env
+[ -f "$BUILD_PATH/packages/.env" ] || $DIR/install-pkg.sh
+
+. $BUILD_PATH/packages/.env
 
 cd $SRC_PATH
 mkdir -p ${BUILD_PATH}/install/include/swss
 
-cd sonic-swss/
-git apply ../patch/swss_pfcwdorch.diff
-cd ../
+# cd sonic-swss/
+# git apply ../patch/swss_pfcwdorch.diff
+# cd ../
 
 # Build sonic-swss-common
 SWSS_COMMON_PATH="${SRC_PATH}/sonic-swss-common"
@@ -49,6 +51,14 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 make install
+
+# TODO: using /usr/bin/patch instead of
+
+sed -i 's|CFLAGS_COMMON+=" -Werror"|#CFLAGS_COMMON+=" -Werror"|g' ${SRC_PATH}/sonic-swss/configure.ac
+
+sed -i 's|string str = counterIdsToStr(c_portStatIds, &sai_serialize_port_stat);|string str = counterIdsToStr(c_portStatIds, static_cast<string (*)(const sai_port_stat_t)>(\&sai_serialize_port_stat));|g' ${SRC_PATH}/sonic-swss/orchagent/pfcwdorch.cpp
+
+sed -i 's|string str = counterIdsToStr(c_queueStatIds, sai_serialize_queue_stat);|string str = counterIdsToStr(c_queueStatIds, static_cast<string (*)(const sai_queue_stat_t)>(\&sai_serialize_queue_stat));|g' ${SRC_PATH}/sonic-swss/orchagent/pfcwdorch.cpp
 
 cd ${BUILD_PATH}
 cmake ${SRC_PATH} -DGTEST_ROOT_DIR=$(pkg-config --variable=prefix googletest)
