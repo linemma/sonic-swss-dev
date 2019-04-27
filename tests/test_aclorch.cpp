ca5866ccedcbef141de2ea5571600c00740bc545
JunYing Yeh <junying_yeh@edge-core.com>
2019-04-27 10:05:53 +0800

    Separate global variables to fake_main.cpp

--- tests/test_aclorch.cpp
+++ tests/test_aclorch.cpp
@@ -1,33 +1,10 @@
 #include "ut_helper.h"
 
-#include "converter.h"
-#include "hiredis.h"
 #include "orchdaemon.h"
-
-#include "saihelper.h"
-
-//#include "aclorch.h"
-#include "saiattributelist.h"
-
-void syncd_apply_view() {}
 
 using namespace std;
 
-/* Global variables */
-sai_object_id_t gVirtualRouterId;
-sai_object_id_t gUnderlayIfId;
-sai_object_id_t gSwitchId = SAI_NULL_OBJECT_ID;
-MacAddress gMacAddress;
-MacAddress gVxlanMacAddress;
-
-#define DEFAULT_BATCH_SIZE 128
-int gBatchSize = DEFAULT_BATCH_SIZE;
-
-bool gSairedisRecord = true;
-bool gSwssRecord = true;
-bool gLogRotate = false;
-ofstream gRecordOfs;
-string gRecordFile;
+extern sai_object_id_t gSwitchId;
 
 extern CrmOrch* gCrmOrch;
 extern PortsOrch* gPortsOrch;
@@ -36,8 +13,8 @@ extern IntfsOrch* gIntfsOrch;
 extern NeighOrch* gNeighOrch;
 extern FdbOrch* gFdbOrch;
 extern AclOrch* gAclOrch;
-MirrorOrch* gMirrorOrch;
-VRFOrch* gVrfOrch;
+extern MirrorOrch* gMirrorOrch;
+extern VRFOrch* gVrfOrch;
 
 extern sai_acl_api_t* sai_acl_api;
 extern sai_switch_api_t* sai_switch_api;
@@ -83,87 +60,6 @@ public:
         Consumer::m_toSync.clear();
     }
 };
-
-// struct Check {
-//     static bool AttrListEq_Miss_objecttype_Dont_Use(const std::vector<sai_attribute_t>& act_attr_list, /*const*/ SaiAttributeList& exp_attr_list)
-//     {
-//         if (act_attr_list.size() != exp_attr_list.get_attr_count()) {
-//             return false;
-//         }
-
-//         auto l = exp_attr_list.get_attr_list();
-//         for (int i = 0; i < exp_attr_list.get_attr_count(); ++i) {
-//             // sai_attribute_t* ptr = &l[i];
-//             // sai_attribute_t& ref = l[i];
-//             auto found = std::find_if(act_attr_list.begin(), act_attr_list.end(), [&](const sai_attribute_t& attr) {
-//                 if (attr.id != l[i].id) {
-//                     return false;
-//                 }
-
-//                 // FIXME: find a way to conver attribute id to type
-//                 // type = idToType(attr.id) // metadata ..
-//                 // switch (type) {
-//                 //     case SAI_ATTR_VALUE_TYPE_BOOL:
-//                 //     ...
-//                 // }
-
-//                 return true;
-//             });
-
-//             if (found == act_attr_list.end()) {
-//                 std::cout << "Can not found " << l[i].id;
-//                 // TODO: Show act_attr_list
-//                 // TODO: Show exp_attr_list
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-
-//     static bool AttrListEq(sai_object_type_t objecttype, const std::vector<sai_attribute_t>& act_attr_list, /*const*/ SaiAttributeList& exp_attr_list)
-//     {
-//         if (act_attr_list.size() != exp_attr_list.get_attr_count()) {
-//             return false;
-//         }
-
-//         auto l = exp_attr_list.get_attr_list();
-//         for (int i = 0; i < exp_attr_list.get_attr_count(); ++i) {
-//             sai_attr_id_t id = exp_attr_list.get_attr_list()[i].id;
-//             auto meta = sai_metadata_get_attr_metadata(objecttype, id);
-
-//             assert(meta != nullptr);
-
-//             char act_buf[0x4000];
-//             char exp_buf[0x4000];
-
-//             auto act_len = sai_serialize_attribute_value(act_buf, meta, &act_attr_list[i].value);
-//             auto exp_len = sai_serialize_attribute_value(exp_buf, meta, &exp_attr_list.get_attr_list()[i].value);
-
-//             // auto act = sai_serialize_attr_value(*meta, act_attr_list[i].value, false);
-//             // auto exp = sai_serialize_attr_value(*meta, &exp_attr_list.get_attr_list()[i].value, false);
-
-//             assert(act_len < sizeof(act_buf));
-//             assert(exp_len < sizeof(exp_buf));
-
-//             if (act_len != exp_len) {
-//                 std::cout << "AttrListEq failed\n";
-//                 std::cout << "Actual:   " << act_buf << "\n";
-//                 std::cout << "Expected: " << exp_buf << "\n";
-//                 return false;
-//             }
-
-//             if (strcmp(act_buf, exp_buf) != 0) {
-//                 std::cout << "AttrListEq failed\n";
-//                 std::cout << "Actual:   " << act_buf << "\n";
-//                 std::cout << "Expected: " << exp_buf << "\n";
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// };
 
 struct AclTestBase : public ::testing::Test {
     std::vector<int32_t*> m_s32list_pool;
--- tests/ut_helper.h
+++ tests/ut_helper.h
@@ -2,6 +2,7 @@
 
 #include "spec_auto_config.h"
 #define LIBVS 1
+#define LIBSAIREDIS 2
 #define WITH_SAI LIBVS // FIXME: from spec_auto_config.h
 
 #include "gtest/gtest.h"
@@ -13,4 +14,9 @@
 #if WITH_SAI == LIBVS
 #include "sai_vs.h"
 #include "sai_vs_state.h"
+#endif
+
+#if WITH_SAI == LIBSAIREDIS
+#include "hiredis.h"
+#include "saihelper.h"
 #endif
