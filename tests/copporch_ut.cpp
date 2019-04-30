1ff2cd0fdf95c1b1bdf509fb2f51273c8815c2c3
chaos_kao <chaos_kao@edge-core.com>
2019-04-30 05:23:21 +0000

    merge attrlist validation

--- tests/copporch_ut.cpp
+++ tests/copporch_ut.cpp
@@ -236,42 +236,6 @@ struct CoppTest : public CoppTestBase {
         auto copp = new CoppOrch(m_app_db.get(), APP_COPP_TABLE_NAME);
         return std::make_shared<CoppOrchHandler>(copp, m_app_db.get());
     }
-
-    bool AttrListEq(const std::vector<sai_attribute_t>& act_attr_list, /*const*/ SaiAttributeList& exp_attr_list)
-    {
-        if (act_attr_list.size() != exp_attr_list.get_attr_count()) {
-            return false;
-        }
-
-        auto l = exp_attr_list.get_attr_list();
-        for (int i = 0; i < exp_attr_list.get_attr_count(); ++i) {
-            // sai_attribute_t* ptr = &l[i];
-            // sai_attribute_t& ref = l[i];
-            auto found = std::find_if(act_attr_list.begin(), act_attr_list.end(), [&](const sai_attribute_t& attr) {
-                if (attr.id != l[i].id) {
-                    return false;
-                }
-
-                // FIXME: find a way to conver attribute id to type
-                // type = idToType(attr.id) // metadata ..
-                // switch (type) {
-                //     case SAI_ATTR_VALUE_TYPE_BOOL:
-                //     ...
-                // }
-
-                return true;
-            });
-
-            if (found == act_attr_list.end()) {
-                std::cout << "Can not found " << l[i].id;
-                // TODO: Show act_attr_list
-                // TODO: Show exp_attr_list
-                return false;
-            }
-        }
-
-        return true;
-    }
 };
 
 struct CoppOrchTest : public CoppTest {
@@ -412,55 +376,6 @@ struct CoppOrchTest : public CoppTest {
         sai_switch_api = nullptr;
     }
 
-    static bool AttrListEq(sai_object_type_t objecttype, const std::vector<sai_attribute_t>& act_attr_list, /*const*/ SaiAttributeList& exp_attr_list)
-    {
-        if (act_attr_list.size() != exp_attr_list.get_attr_count()) {
-            return false;
-        }
-
-        auto l = exp_attr_list.get_attr_list();
-        for (int i = 0; i < exp_attr_list.get_attr_count(); ++i) {
-            sai_attr_id_t id = exp_attr_list.get_attr_list()[i].id;
-
-            // ignore oid check
-            if (objecttype == SAI_OBJECT_TYPE_HOSTIF_TRAP && id == SAI_HOSTIF_TRAP_ATTR_TRAP_GROUP) {
-                continue;
-            }
-
-            auto meta = sai_metadata_get_attr_metadata(objecttype, id);
-
-            assert(meta != nullptr);
-
-            char act_buf[0x4000];
-            char exp_buf[0x4000];
-
-            auto act_len = sai_serialize_attribute_value(act_buf, meta, &act_attr_list[i].value);
-            auto exp_len = sai_serialize_attribute_value(exp_buf, meta, &exp_attr_list.get_attr_list()[i].value);
-
-            // auto act = sai_serialize_attr_value(*meta, act_attr_list[i].value, false);
-            // auto exp = sai_serialize_attr_value(*meta, &exp_attr_list.get_attr_list()[i].value, false);
-
-            assert(act_len < sizeof(act_buf));
-            assert(exp_len < sizeof(exp_buf));
-
-            if (act_len != exp_len) {
-                std::cout << "AttrListEq failed\n";
-                std::cout << "Actual:   " << act_buf << "\n";
-                std::cout << "Expected: " << exp_buf << "\n";
-                return false;
-            }
-
-            if (strcmp(act_buf, exp_buf) != 0) {
-                std::cout << "AttrListEq failed\n";
-                std::cout << "Actual:   " << act_buf << "\n";
-                std::cout << "Expected: " << exp_buf << "\n";
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     std::shared_ptr<SaiAttributeList> getTrapGroupAttributeList(const vector<FieldValueTuple> rule_values)
     {
         std::vector<swss::FieldValueTuple> fields;
@@ -531,7 +446,7 @@ struct CoppOrchTest : public CoppTest {
             return false;
         }
 
-        auto b_attr_eq = AttrListEq(trapGroupObjectType, trap_group_act_attr, exp_group_attr_list);
+        auto b_attr_eq = Check::AttrListEq(trapGroupObjectType, trap_group_act_attr, exp_group_attr_list);
         if (!b_attr_eq) {
             return false;
         }
@@ -562,7 +477,7 @@ struct CoppOrchTest : public CoppTest {
             return false;
         }
 
-        auto b_attr_eq = AttrListEq(trapObjectType, trap_act_attr, exp_trap_attr_list);
+        auto b_attr_eq = Check::AttrListEq(trapObjectType, trap_act_attr, exp_trap_attr_list);
         if (!b_attr_eq) {
             return false;
         }
